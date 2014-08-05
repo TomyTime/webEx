f08896c3846ddfb55a99f8cbbb2123f4bdd2cc06
TomyTime <muskert@163.com>
2014-08-05 18:45:43 +0800

    config browser-sync error

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt) {
+
+  // 项目配置
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    watch: {
+
+    },
+
+    browser_sync: {
+      files: {
+        src : [
+          'public/stylesheets/*.css',
+          'public/images/*',
+          'public/javascripts/*.js',
+          'view/*'
+        ]
+      },
+      options: {
+        watchTask: true
+      }
+    },
+    cssmin: {
+      minify: {
+        expand: true,
+        cwd: 'public/stylesheets/',
+        src: ['*.css', '!*.min.css'],
+        dest: 'public/stylesheets/',
+        ext: '.min.css'
+      }/*,
+      combine: {
+        files: {
+          'css/out.min.css': ['pub/part1.min.css', 'css/part2.min.css']
+        }
+      }*/
+    }
+  });
+
+  // 加载Grunt插件
+  grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  // 注册grunt默认任务
+  grunt.registerTask('default', ["browser_sync"]);
+};
